454491e9269566c7d1bef9d1fa56907182cb00da
gescofet09 <gabriela.escofet09@gmail.com>
2026-02-04 12:38:31 +0000

    p

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const list = document.getElementById('list');
 boton.addEventListener('click', () => {
   const frag = document.createDocumentFragment();
 
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 11; i++) {
     const li = document.createElement('li');
     li.textContent = 'Elemento ' + i;
     frag.appendChild(li);
