66b0b039417324c0b432b289b01524679f9714ff
gescofet09 <gabriela.escofet09@gmail.com>
2026-02-05 08:12:13 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
-const boton = document.getElementById('boton');
-const list = document.getElementById('list');
-
-boton.addEventListener('click', () => {
-  const frag = document.createDocumentFragment();
-
-  for (let i = 0; i < 11; i++) {
-    const li = document.createElement('li');
-    li.textContent = 'Elemento ' + i;
-    frag.appendChild(li);
-  }
-
-  list.appendChild(frag);
-
-  // Elimina el evento para que no vuelva a ejecutarse
-  boton.remove();
-});
+for (let i = 0; i < 100; i++) {
+  const li = document.createElement('li');
+  li.textContent = 'Elemento ' + i;
+  frag.appendChild(li);
+}
